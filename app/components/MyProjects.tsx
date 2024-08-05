2a68eec13e2ad6363e9d903b0bfcb043388d9df2
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-08-05 19:30:35 +0200

    card

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { Card } from './ui/Card';
 import { Title } from './ui/Title';
 
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-
-// import Slider from 'react-slick';
-
 export const MyProjects = ({ title }) => {
   var settings = {
     dots: true,
@@ -18,16 +13,30 @@ export const MyProjects = ({ title }) => {
   return (
     <div className="flex flex-col items-center p-3">
       <Title title={title} />
-      {/* <Slider
-        className=" bg-background2 flex flex-col min-h-96 w-80 md:w-tablet rounded-lg m-auto p-2  "
-        {...settings}
-      > */}
+
       <div>
-        <Card titleCard="GiftList" text="aaa" link="aaalink" />
-        <Card titleCard="Harry Potter" text="aaa" link="aaalink" />
-        <Card titleCard="My To Do List" text="aaa" link="aaalink" />
+        <Card
+          titleCard="GiftList"
+          text="Interactive application for kids that allows them to create a gift list."
+          link="https://gift-list-eight.vercel.app/"
+          tech="HTML5, JavaScript, React, NextJS, TypeScript, NodeJS, Tailwind"
+          image={'/giftList.png'}
+        />
+        <Card
+          titleCard="Harry Potter"
+          text="Web search engine for characters from the famous Harry Potter series."
+          link="https://nadinestrella.github.io/HarryPotter/"
+          tech="HTML5, SASS, JavaScript, React"
+          image={'/harryPotter.png'}
+        />
+        <Card
+          titleCard="My To Do List"
+          text="It allows y﻿ou to efficiently organize and keep monitor your tasks."
+          link="https://nadinestrella.github.io/myToDoList/"
+          tech="HTML5, SASS, JavaScript"
+          image={'/myToDoList.png'}
+        />
       </div>
-      {/* </Slider> */}
     </div>
   );
 };
--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
-export const Card = ({ titleCard, text, link }) => {
+export const Card = ({ titleCard, text, link, tech, image }) => {
   return (
-    <article>
-      <h4>{titleCard}</h4>
-      <Image width={100} height={100} alt="project photo" />
-      <p>{text} </p>
-      <span>{link}</span>
+    <article className="border-2 rounded w-full bg-zinc-200 p-2 ">
+      <h4 className="flex flex-col items-center font-semibold p-3">
+        {titleCard}
+      </h4>
+      <Image
+        width={200}
+        height={200}
+        alt="project photo"
+        src={image}
+        className="p-3"
+      />
+      <p className="p-3">{text} </p>
+      <span className="p-3">{tech}</span>
+      <a className="p-3 block">{link}</a>
     </article>
   );
 };
