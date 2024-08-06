debd90995aa2d87db583c1abace1b0e85c3078aa
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-08-06 17:05:26 +0200

    icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center relative -bottom-9 ">
+    <div className="flex flex-col justify-center items-center relative -bottom-9 bg-background p-4 gap-2 text-gray-500 ">
       <p>Nadine Estrella | 2024</p>
-      <div>
-        <span>linkedin</span>
-        <span>github</span>
-        <span>email</span>
+      <div className="flex flex-row gap-5">
+        <a
+          href="https://www.linkedin.com/in/nadinestrellavacas/"
+          target="_blank"
+          className="hover:text-background3"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+
+        <a
+          href="https://github.com/nadinestrella"
+          target="_blank"
+          className="hover:text-background3"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] });
 
